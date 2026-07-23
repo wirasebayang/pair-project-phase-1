e83b86b6af76126378e8c6f629fdd80a50caef40
salsabila <salsabilaa1470@gmail.com>
2026-07-23 11:52:45 +0700

    migration

---
+++ pair_project/migrations/20260723010003-create-tutoring-service.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('TutoringServices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+        model: 'Categories',
+        key: 'id'
+      }, 
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+        model: 'Users',
+        key: 'id'
+      }, 
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('TutoringServices');
+  }
+};
---
+++ pair_project/models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(models.UserProfile, {
+  onDelete: 'RESTRICT',
+  onUpdate: 'RESTRICT',
+});
+
+User.hasMany(models.TutoringService, {
+  foreignKey: 'UserId',
+});
+
+User.belongsToMany(models.TutoringService, { through: 'Booking' , foreignKey : 'StudentId'});
+    }
+  }
+  User.init({
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
